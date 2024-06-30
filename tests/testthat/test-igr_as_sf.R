938f0fb432d22cd8a8390f6e9a36f7737a7e3978
John Kennedy <john@digitalnature.ie>
2024-07-01 00:05:19 +0100

    test: expanded test cases

---
+++ tests/testthat/test-igr_as_sf.R
@@ -0,0 +1,18 @@
+library(sf)
+
+x1 <- data.frame(igr = c("A"))
+x2 <- data.frame(igr = c("A", "Z90"))
+x3 <- data.frame(igr = c("A", "Z90"), foo = c("foo_A", "foo_Z90"))
+
+test_that("basic conversions", {
+  expect_equal(igr_as_sf(x1, "igr"), st_as_sf(data.frame(igr = c("A"), x = c(0), y = c(400000)), crs = 29903, coords = c("x", "y")))
+
+  expect_equal(igr_as_sf(x2, "igr"), st_as_sf(data.frame(igr = c("A", "Z90"), x = c(0, 490000), y = c(400000, 0)), crs = 29903, coords = c("x", "y")))
+})
+
+test_that("igrefs", {
+    expect_equal(igr_as_sf(x3, "igr"), st_as_sf(data.frame(igr = c("A", "Z90"), foo = c("foo_A", "foo_Z90"), x = c(0, 490000), y = c(400000, 0)), crs = 29903, coords = c("x", "y")))
+# test for invalid igr    
+})
+
+#TODO Add more tests
--- tests/testthat/test-igr_to_ig.R
+++ tests/testthat/test-igr_to_ig.R
@@ -1,21 +1,48 @@
 test_that("basic conversions", {
-  # expect_equal(igr_to_ig("A"), c(e=0,n=400000))
-  # expect_equal(igr_to_ig("A00"), c(e=0,n=400000))
-  # expect_equal(igr_to_ig("A09"), c(e=0,n=490000))
-  # 
-  # expect_equal(igr_to_ig("Z00"), c(e=400000,n=0))
-  # expect_equal(igr_to_ig("Z90"), c(e=490000,n=0))
-  # 
-  # expect_equal(igr_to_ig(c("A00", "Z90")), c(e1=0,e2=490000,n1=400000, n2=0))
+  expect_equal(igr_to_ig("A"), list(x=0,y=400000))
+  expect_equal(igr_to_ig("A00"), list(x=0,y=400000))
+  expect_equal(igr_to_ig("A09"), list(x=0,y=490000))
+
+  expect_equal(igr_to_ig("Z00"), list(x=400000,y=0))
+  expect_equal(igr_to_ig("Z90"), list(x=490000,y=0))
+
+  expect_equal(igr_to_ig(c("A00", "Z90")), list(x=c(0,490000), y = c(400000, 0)))
   
+  expect_equal(igr_to_ig(NULL), list(x = double(), y = double()))
+  expect_equal(igr_to_ig(c("A00", NULL)), list(x = c(0,double()), y = c(400000,double())))
+})
+
+test_that("all resolutions 100km > 1m", {
+  expect_equal(igr_to_ig("A"),           list(x=000000,y=400000))
+  expect_equal(igr_to_ig("A11"),         list(x=010000,y=410000))
+  expect_equal(igr_to_ig("A1212"),       list(x=012000,y=412000))
+  expect_equal(igr_to_ig("A123123"),     list(x=012300,y=412300))
+  expect_equal(igr_to_ig("A12341234"),   list(x=012340,y=412340))
+  expect_equal(igr_to_ig("A1234512345"), list(x=012345,y=412345))
+  
+  expect_equal(igr_to_ig(c("A","B11")),            list(x=c(000000,110000),y=c(400000,410000)))
+  expect_equal(igr_to_ig(c("A11", "B11")),         list(x=c(010000,110000),y=c(410000,410000)))
+  expect_equal(igr_to_ig(c("A1212", "B11")),       list(x=c(012000,110000),y=c(412000,410000)))
+  expect_equal(igr_to_ig(c("A123123", "B11")),     list(x=c(012300,110000),y=c(412300,410000)))
+  expect_equal(igr_to_ig(c("A12341234", "B11")),   list(x=c(012340,110000),y=c(412340,410000)))
+  expect_equal(igr_to_ig(c("A1234512345", "B11")), list(x=c(012345,110000),y=c(412345,410000)))
+})
+
+test_that("rename coordinates", {
+  expect_equal(igr_to_ig("A00", coords = c("x", "y")), list(x=0,y=400000))   
+  expect_equal(igr_to_ig("A00", coords = c("e", "n")), list(e=0,n=400000))
+  expect_equal(igr_to_ig(c("A00", "Z90"), coords = c("e", "n")), list(e=c(0,490000), n = c(400000, 0)))
 })
 
 test_that("catch invalid grid references", {
-  
+  expect_error(igr_to_ig(""), class = "bad_input")
+  expect_error(igr_to_ig(NA_character_), class = "bad_input")
   expect_error(igr_to_ig("A0"), class = "bad_input")
+  expect_error(igr_to_ig("A123456123456"), class = "bad_input")
   expect_error(igr_to_ig(c("A00", "B0")), class = "bad_input")
   expect_error(igr_to_ig("I00"), class = "bad_input")
   expect_error(igr_to_ig("Ax0"), class = "bad_input")
   expect_error(igr_to_ig("A0x"), class = "bad_input")
-  
 })
+
+#TODO Add more tests
